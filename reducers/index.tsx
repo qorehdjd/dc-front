c712f8a8e0f41101f7952f67d2969fdd0e6aca98
남궁 <qorehdjd@naver.com>
2024-03-26 15:09:00 +0900

    revise/baseUrl

--- reducers/index.tsx
+++ reducers/index.tsx
@@ -4,7 +4,7 @@ import axios from 'axios';
 import userSlice from './user';
 import postSlice from './post';
 
-axios.defaults.baseURL = 'https://api.portpolio-dcinside.site';
+axios.defaults.baseURL = 'https://api.dcinside-web.shop';
 axios.defaults.withCredentials = true;
 const rootReducer = combineReducers({
   user: userSlice.reducer,
