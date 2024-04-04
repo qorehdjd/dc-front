14e35703c5af283cc6ea954d4b0683a5f15e04f5
남궁 <qorehdjd@naver.com>
2024-04-04 22:27:14 +0900

    revise/baseURL

--- reducers/index.tsx
+++ reducers/index.tsx
@@ -4,8 +4,7 @@ import axios from 'axios';
 import userSlice from './user';
 import postSlice from './post';
 
-// axios.defaults.baseURL = 'https://api.dcinside-web.shop';
-axios.defaults.baseURL = 'http://localhost:3090';
+axios.defaults.baseURL = 'https://api.dcinside-web.shop';
 axios.defaults.withCredentials = true;
 const rootReducer = combineReducers({
   user: userSlice.reducer,
